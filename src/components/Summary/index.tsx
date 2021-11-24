995eb1c468698dc22e2969484fd1fc5319b248ac
bi053344 <bianca.inacio@intelbras.com.br>
2021-11-23 22:21:43 -0300

    fix: tornando summary dinamico

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,38 @@
+import {useEffect, useState} from 'react';
 import {GoFile, GoNote, GoGraph} from 'react-icons/go'
 import {Container} from "./styles";
+import api from '../../services/api';
+
+interface Activity {
+    id: string;
+    name: string;
+    grade: number;
+    activity_date: Date;
+}
+
+interface CourseUnit {
+    id: string;
+    name: string;
+    description: string;
+}
 
 export function Summary() {
+
+    const [activities, setActivities] = useState<Activity[]>([])
+    const [courseUnits, setcourseUnits] = useState<CourseUnit[]>([])
+
+    useEffect(() => {
+
+        api.get('/activity')
+            .then(response => setActivities(response.data))
+    },[])
+
+    useEffect(() => {
+
+        api.get('/courseunit')
+            .then(response => setcourseUnits(response.data))
+    },[])
+
     return(
         <Container>
             <div>
@@ -10,7 +41,7 @@ export function Summary() {
                     <GoFile size={40} />
                 </header>
                 <strong>
-                    25
+                    {courseUnits.length}
                 </strong>
             </div>
             <div>
@@ -19,7 +50,7 @@ export function Summary() {
                         <GoNote size={40} />
                     </header>
                     <strong>
-                        80
+                        {activities.length}
                     </strong>
             </div>
             <div className="highlight-background">
@@ -28,7 +59,9 @@ export function Summary() {
                         <GoGraph size={40} />
                     </header>
                     <strong>
-                        9.17
+                        {Number(activities.reduce((average,activity) => {
+                        return average + Number(activity.grade)
+                        },0)/activities.length).toFixed(2)}
                     </strong>
             </div>
         </Container>
